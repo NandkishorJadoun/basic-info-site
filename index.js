912a4b61b477650e3f6adcb3988661af5e1354c0
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-09-19 19:12:43 +0530

    fix: remove the repeated code & add res.writeHead for responses

--- index.js
+++ index.js
@@ -3,39 +3,29 @@ import fs from "fs";
 
 const PORT = 8080;
 
-http
-  .createServer((req, res) => {
-    switch (req.url) {
-      case "/":
-        fs.readFile("index.html", "utf-8", (err, data) => {
-          if (err) throw err;
-          res.write(data);
-          res.end();
-        });
-        break;
+const server = http.createServer((req, res) => {
+  let page = "404.html";
+  switch (req.url) {
+    case "/":
+      page = "index.html";
+      break;
 
-      case "/about":
-        fs.readFile("about.html", "utf-8", (err, data) => {
-          if (err) throw err;
-          res.write(data);
-          res.end();
-        });
-        break;
+    case "/about":
+      page = "about.html";
+      break;
 
-      case "/contact-me":
-        fs.readFile("contact-me.html", "utf-8", (err, data) => {
-          if (err) throw err;
-          res.write(data);
-          res.end();
-        });
-        break;
+    case "/contact-me":
+      page = "contact-me.html";
+      break;
+  }
 
-      default:
-        fs.readFile("404.html", "utf-8", (err, data) => {
-          if (err) throw err;
-          res.write(data);
-          res.end();
-        });
-    }
-  })
-  .listen(PORT, () => console.log("server is running"));
+  const status = page === "404.html" ? 400 : 200;
+
+  fs.readFile(page, "utf-8", (err, data) => {
+    if (err) throw err;
+    res.writeHead(status, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+});
+
+server.listen(PORT, () => console.log("server is running..."));
